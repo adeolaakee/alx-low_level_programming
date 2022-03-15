1b9a9f361ad6baf5bd5d89220496269f5211786a
adeolaakee <adeolaakee@gmail.com>
2022-03-15 18:31:12 +0000

    pushing through

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,22 @@
 #include <unistd.h>
 #include "main.h"
+
+/**
+ * print_alphabet  - this is the entry point
+ * This scripts print the lowercase alphabet
+ *
+ * Return: Always (0)
+ */
+
 void print_alphabet(void)
 {
 	char ch;
 
 	ch = 'a';
-	for (ch <= 'z' ch++)
+	while (ch <= 'z')
 	{
 		_putchar(ch);
+		ch++;
 	}
 	_putchar('\n');
 	return (0);
