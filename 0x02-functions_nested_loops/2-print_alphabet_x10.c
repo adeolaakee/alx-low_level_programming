179c1abfb38604c905555599357832738456871e
adeolaakee <adeolaakee@gmail.com>
2022-03-15 19:24:41 +0000

    pushing through

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
- * print alphabet - this is the entry point
- * this code prints lowercase alphabet 10times
+ * print_alphabet_x10 - this is the entry point
+ * print code prints lowercase alphabet 10times
  */
 void print_alphabet_x10(void)
 {
 	char ch;
-	int i = 0;
-	
+	int i = 0
+		;
 	while (i < 0)
 	{
-		ch = 'a'
-		while (ch <= 'z');
+		ch = 'a';
+		while (ch <= 'z')
 		_putchar(ch);
 		ch++;
 	}
 	_putchar('\n');
+}
