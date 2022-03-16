8d12defd2a5d01d1afc9a6e8486f1db5a0aefd5f
adeolaakee <adeolaakee@gmail.com>
2022-03-16 09:20:51 -0700

    the making of Gold

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,2 +1,29 @@
 #include "main.h"
+/**
+ * times_table - prints multiplication time from
+ * 0 to 9
+ *
+ */
 
+void times_table(void)
+{
+	int n, j, k;
+
+	n = 0;
+
+	while (n <= 9)
+	{
+		j = 0
+		while (j <= 9)
+		{
+			i = j * n;
+			j++;
+
+			time_table(i)
+			_putchar(",");
+			_putchar(" ");
+		}
+		_putchar("\n");
+		n++;
+	}
+}
