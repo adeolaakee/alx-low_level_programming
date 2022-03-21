a1c7cb0561d27b92d21c84628ea54d03bd3f1fe4
adeolaakee <adeolaakee@gmail.com>
2022-03-21 02:42:01 -0700

    pointer, string and array

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,9 +3,9 @@
 /**
  * reset_to_98 - This takes the pointer to a parameter called n
  * and update its value to 98
+ * @n: pointer to the variable to update
  */
 void reset_to_98(int *n)
 {
-	int n = 98;
-	*p = &n;
+	*n = 98;
 }
